bed28eff872f8ffa9695c4d20181dc707c379b0a
Lê Quyết Chiến <39990120+chien334@users.noreply.github.com>
2026-06-01 20:18:20 +0700

    feat(migration): upgrade frontend to Angular 11 with clean compilation

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -95,7 +95,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     ReactiveFormsModule,
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(AppRoutingConstant),
+    RouterModule.forRoot(AppRoutingConstant, { relativeLinkResolution: 'legacy' }),
     SharedModule,
     // NotesModule,
     HttpClientModule,
